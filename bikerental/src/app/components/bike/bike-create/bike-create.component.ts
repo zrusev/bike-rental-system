845e0aeb9258a590f26424a04b7bed1a6c161a2d
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2019-04-20 11:16:47 +0300

    bike-create form validation

--- bikerental/src/app/components/bike/bike-create/bike-create.component.ts
+++ bikerental/src/app/components/bike/bike-create/bike-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit,  DoCheck } from '@angular/core';
 import { ICoords } from '../../shared/models/ICoords';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
@@ -7,21 +7,26 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: './bike-create.component.html',
   styleUrls: ['./bike-create.component.css']
 })
-export class BikeCreateComponent implements OnInit {
-  coordinates: ICoords;
+export class BikeCreateComponent implements OnInit, DoCheck {
+  coordinates: ICoords = {lat: 42.6978634, lng: 23.3221789};
   form: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.form = this.fb.group({
-    name: ['', [Validators.required, Validators.maxLength(10)]],
-    description: ['', [Validators.required, Validators.minLength(10)]],
-    imageUrl: ['', [Validators.required, Validators.pattern(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i)]],
-    privacyLevel: ['', [Validators.required, Validators.pattern(/[Public|Private]/)]],
-    latitude: ['', Validators.required],
-    longitude: ['', Validators.required]
+      name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+      imageUrl: ['', [Validators.required, Validators.pattern(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i)]],
+      privacyLevel: ['', [Validators.required, Validators.pattern(/^(Public)|(Private)$/)]],
+      latitude: ['', Validators.pattern(/^\d*\.?\d*$/)],
+      longitude: ['', Validators.pattern(/^\d*\.?\d*$/)]
     });
+  }
+
+  ngDoCheck() {
+    this.form.controls['latitude'].setValue(this.coordinates.lat);
+    this.form.controls['longitude'].setValue(this.coordinates.lng);
   }
 
   createBike() {
@@ -29,8 +34,6 @@ export class BikeCreateComponent implements OnInit {
   }
 
   get f() { return this.form.controls; }
-
-  get invalid() { return this.form.invalid; }
 
   onMarked(coords: ICoords) { this.coordinates = coords; }
 }
